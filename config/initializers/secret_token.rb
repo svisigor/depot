9a6bcf96e13fef3b93bf144be653b748c23f8419
svisigor <svisigor@mail.ru>
2013-08-14 08:30:43 +0300

    Depot Agile ROR Dev 4th edition

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depo2::Application.config.secret_key_base = '78e48a80cd438e9f17847796b2196236a1ce92882eb56ef973b5a729571d6c3ececbdc754832885af1a311f89d7e6541b2bb9546482fc031ad509581161d372d'
